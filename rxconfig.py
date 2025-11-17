dc2edb2aa200f18766dbe3047033cf01fa7d86ef
Daniel Paredes <danielparedes@ciencias.unam.mx>
2025-11-16 22:26:48 -0600

    fix?: posible solucion al error de websocket

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,10 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="app", plugins=[rx.plugins.TailwindV3Plugin(), rx.plugins.SitemapPlugin()]
+    app_name="app",
+    plugins=[rx.plugins.TailwindV3Plugin()],
+    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
+    telemetry_enabled=False,
+    reconnect_timeout=0,
+    deploy_url="",
 )
