8cb40eeffb3e4653b08f08bb1290fba8f5505ad7
Daniel Paredes <danielparedes@ciencias.unam.mx>
2025-11-18 19:44:26 -0600

    test: upgrade a reflex 0.8.20 y ajustes de uv para Vercel

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
-python3 -m venv .venv
+wget -qO- https://astral.sh/uv/install.sh | sh
+uv init
+uv add reflex==0.8.20
 source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-rm -rf public
 reflex init
 reflex export
-unzip backend.zip -d public
 unzip frontend.zip -d public
 rm -f frontend.zip
-rm -f backend.zip
 deactivate
