6a5954c8b1e9f7b45ec2c933724153fc93dcfc8a
Daniel Paredes <danielparedes@ciencias.unam.mx>
2025-11-18 22:38:30 -0600

    test: y si desplegamos todo sin exportar?

--- build.sh
+++ build.sh
@@ -10,15 +10,4 @@ rm -rf public
 mkdir -p public
 
 reflex init
-reflex export --frontend-only
-
-# Check if zip files exist before unzipping
-if [ -f "frontend.zip" ]; then
-    unzip -o frontend.zip -d public
-    rm -f frontend.zip
-else
-    echo "Error: frontend.zip not found"
-    exit 1
-fi
-
-deactivate
+reflex run
