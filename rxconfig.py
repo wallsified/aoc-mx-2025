e5bc407f5eff114863e30191d6489b9c596e63dc
Daniel Paredes <danielparedes@ciencias.unam.mx>
2025-11-18 19:49:51 -0600

    test: otro intento de arreglar el problema del websocket

--- rxconfig.py
+++ rxconfig.py
@@ -6,4 +6,6 @@ config = rx.Config(
     disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
     enable_state=False,
     backend_exception_handler=None,
+    api_url="",  # Disable API/WebSocket connections for static export
+    deploy_url="",  # Prevent any backend URL references
 )
