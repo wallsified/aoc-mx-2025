e41b23c5ab484bd40957ad1a0cbc95175f38cce2
Daniel Paredes <danielparedes@ciencias.unam.mx>
2025-11-18 20:45:51 -0600

    .

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,8 @@ config = rx.Config(
     app_name="app",
     plugins=[rx.plugins.TailwindV3Plugin()],
     disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
-    enable_state=False,
-    backend_exception_handler=None,
-    api_url="",  # Disable backend API/WebSocket connections for static export
+    frontend_port=3000,
+    backend_port=None,
+    telemetry_enabled=False,
+    is_reflex_cloud=False
 )
