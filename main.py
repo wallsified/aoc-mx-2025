5dfe6cf2b5d3e6f44557f59550160a49f2a22a6a
Daniel Paredes <danielparedes@ciencias.unam.mx>
2025-11-18 23:11:15 -0600

    test: no creo que añadir el puerto y el host obvios funcione o si?

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from aoc-mx-2025!")
-
-
-if __name__ == "__main__":
-    main()
--- rxconfig.py
+++ rxconfig.py
@@ -4,4 +4,6 @@ config = rx.Config(
     app_name="app",
     plugins=[rx.plugins.TailwindV4Plugin(), rx.plugins.sitemap.SitemapPlugin()],
     # disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
+    backend_host="0.0.0.0",
+    backend_port=8000,
 )
