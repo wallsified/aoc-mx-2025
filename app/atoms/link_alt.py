496d144d1c95eb3a9cec88553411b786d979505f
Daniel Paredes <danielparedes@ciencias.unam.mx>
2025-11-21 12:09:42 -0600

    style: cambio de color a los links de los retos.

--- app/atoms/link_alt.py
+++ app/atoms/link_alt.py
@@ -3,12 +3,12 @@ from reflex import el, Component, icon, hstack
 
 def link_alt(text: str, href: str) -> Component:
     return hstack(
-        icon("calendar-days", size=30, color="white"),
+        icon("calendar-days", size=30, color="#e6be9a"),
         el.a(
             f"{text}",
             href=href,
             target="_blank",
-            class_name="text-xl text-white-400 hover:text-[#6f0f11] transition-colors",
+            class_name="text-xl text-white-600 hover:text-[#6f0f11] transition-colors",
         ),
         spacing="3",
         align="center",
